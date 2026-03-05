52d440776277bfab14922236e8085270878fa31f
abra25 <solidy789gmail.com>
2026-03-05 00:39:22 -0800

    fix frontend pi

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https:/ebirth-production.up.railway.app',
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://ebirth-production.up.railway.app/api",
 });
 
 export default api;
